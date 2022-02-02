196052192519743e4594ce71c391d1a689ca1658
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-02-02 10:28:05 -0500

    commit tree d0dfbb98bd60ec2354db8bcebc1ffbf176b32bf9

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(array) {
+    if (array.find(findWin) === undefined) {
+        return undefined;
+    }
+    else {
+        return array.find(findWin).year;
+    }
+}
+
+function findWin (element) {
+    return element.result === 'W';
+}
